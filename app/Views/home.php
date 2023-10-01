264a8eda85fa077083399264f5dc8fec554eaa44
ashlyomanada <128467630+ashlyomanada@users.noreply.github.com>
2023-10-01 12:55:51 +0800

    ecommerce

--- app/Views/home.php
+++
@@ -1,3 +0,0 @@
-<!-- New Arrivals section start -->
-<a href="\save" class="btn btn-success">Go to website</a>
-<!-- New Arrivals section end -->
--- public/welcome_message.php
+++
@@ -1,5 +0,0 @@
-<?= $this->include('include/top')?>
-<body>
-<?= $this->include('include/header')?>
-<?= $this->include('include/banner')?>
-</body>
